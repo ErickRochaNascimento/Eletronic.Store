82c2cc6d2602fbdfa6314d5ec28abd18e01145bf
Erick Nascimento <erickrochaluciano@gmail.com>
2025-09-06 19:37:55 +0000

    Criação JS

--- scripts.js
+++ scripts.js
@@ -13,20 +13,41 @@ function update(direction) {
     document.querySelector('.item.active').classList.remove('active')
     document.querySelector('.dot.active').classList.remove('active')
 
-    if(direction > 0 ){
-
-    }else if(direction < 0){
-        
+    if (direction > 0) {
+        active = active + 1;
+        if (active === total) {
+            active = 0
+        }
     }
+    else if (direction < 0) {
+        active = active - 1;
+        if (active < 0) {
+            active = total - 1;
+        }
+    }
+    items[active].classList.add('active')
+    dots[active].classList.add('active')
+
+    numberIndicator.textContent = String(active + 1).padStart(2,'0')
+
+
+
+
+
+
 }
 
+clearInterval(timer)
+timer =  setInterval(() => {
+    update(1)
+}, 5000);
 
 
-prevButton.addEventListener('click', function() {
+prevButton.addEventListener('click', function () {
     update(-1)
 
 })
-nextButton.addEventListener('click', function() {
+nextButton.addEventListener('click', function () {
     update(1)
 
 })
